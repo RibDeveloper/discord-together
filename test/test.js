53fe11be6031037833c7cdf17863d24e134c2e0a
RibDeveloper <83517767+RibDeveloper@users.noreply.github.com>
2021-05-27 09:20:05 -0400

    Update test.js

--- test/test.js
+++ test/test.js
@@ -7,7 +7,7 @@ client.discordTogether = new DiscordTogether(client);
 client.on('message', async message => {
     if (message.content === 'start') {
         if(message.member.voice.channel) {
-            client.discordTogether.createTogetherCode(message.member.voice.channelID, 'youtube').then(async invite => {
+            client.discordTogether.createTogetherCode(message.member.voice.channelID, 'youtube').then(invite => {
                 return message.channel.send(`${invite.code}`); // Click the blue link !
             });
         };
